d7112ffb5fa108c425b57756ab7cba6498f739df
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2023-12-11 23:44:18 +0900

    기능 구현 완료

--- Client/main.c
+++ Client/main.c
@@ -5,6 +5,11 @@ typedef struct
     GtkWidget *entry, *textview;
 } Widgets;
 
+typedef struct {
+    GtkWidget *id_entry;
+    GtkWidget *pwd_entry;
+} LoginWidgets;
+
 /* GtkEntry 로부터 GtkTextView 로 텍스트 삽입 */
 static void insert_text(GtkButton *button, Widgets *w)
 {
@@ -112,43 +117,108 @@ void show_new_window(GtkWidget *widget, gpointer data) {
 
 
 // 로그인 버튼을 눌렀을 때 호출되는 함수
-void on_login_button_clicked(GtkWidget *widget, gpointer data) {
-    // 여기에 실제 로그인 처리 로직을 추가할 수 있음
+// void on_login_button_clicked(GtkWidget *widget, gpointer data) {
+//     // 여기에 실제 로그인 처리 로직을 추가할 수 있음
 
-    // 로그인 후 새로운 윈도우를 띄움
-    show_new_window(widget, data);
+//     // 로그인 후 새로운 윈도우를 띄움
+//     show_new_window(widget, data);
+// }
+
+void login_button_clicked(GtkWidget *widget, gpointer data) {
+    LoginWidgets *login_widgets = (LoginWidgets *)data;
+    const char *id = gtk_entry_get_text(GTK_ENTRY(login_widgets->id_entry));
+    const char *pwd = gtk_entry_get_text(GTK_ENTRY(login_widgets->pwd_entry));
+
+    // 간단한 로그인 체크
+    if (strcmp(id, "test") == 0 && strcmp(pwd, "1234") == 0) {
+        // 로그인 성공 시 새로운 창 열기
+        show_new_window(widget, NULL);
+    } else {
+        // 로그인 실패 메시지 표시
+        GtkWidget *error_dialog = gtk_message_dialog_new(GTK_WINDOW(gtk_widget_get_toplevel(widget)),
+                                                         GTK_DIALOG_DESTROY_WITH_PARENT,
+                                                         GTK_MESSAGE_ERROR,
+                                                         GTK_BUTTONS_OK,
+                                                         "Login failed. Please check your ID and password.");
+        gtk_dialog_run(GTK_DIALOG(error_dialog));
+        gtk_widget_destroy(error_dialog);
+    }
 }
 
 int main(int argc, char *argv[]) {
     GtkWidget *window;
-    GtkWidget *grid;
+    GtkWidget *id_entry;
+    GtkWidget *pwd_entry;
     GtkWidget *login_button;
 
-    // GTK 초기화
     gtk_init(&argc, &argv);
 
-    // 메인 윈도우 생성
+    LoginWidgets *login_widgets = g_slice_new(LoginWidgets);
+
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Login Window");
+    gtk_window_set_title(GTK_WINDOW(window), "Login Example");
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // 그리드 생성
-    grid = gtk_grid_new();
-    gtk_container_add(GTK_CONTAINER(window), grid);
-
-    // 로그인 버튼 생성 및 설정
+    id_entry = gtk_entry_new();
+    pwd_entry = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(pwd_entry), FALSE); // 비밀번호 입력을 위해 가려주기
     login_button = gtk_button_new_with_label("Login");
-    g_signal_connect(login_button, "clicked", G_CALLBACK(on_login_button_clicked), NULL);
-    gtk_grid_attach(GTK_GRID(grid), login_button, 1, 1, 1, 1);
 
-    // 윈도우 크기 설정
-    gtk_widget_set_size_request(window, 300, 200);
+    // 로그인 버튼 클릭 시 동작을 설정
+    g_signal_connect(login_button, "clicked", G_CALLBACK(login_button_clicked), login_widgets);
 
-    // 윈도우를 보이게 함
+    // UI 레이아웃 설정
+    GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+    gtk_box_pack_start(GTK_BOX(box), id_entry, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box), pwd_entry, FALSE, FALSE, 0);
+    gtk_box_pack_start(GTK_BOX(box), login_button, FALSE, FALSE, 0);
+
+    gtk_container_add(GTK_CONTAINER(window), box);
+
+    // 구조체에 위젯 저장
+    login_widgets->id_entry = id_entry;
+    login_widgets->pwd_entry = pwd_entry;
+
     gtk_widget_show_all(window);
 
-    // GTK 메인 루프 실행
     gtk_main();
+
+    // 구조체 메모리 해제
+    g_slice_free(LoginWidgets, login_widgets);
 
     return 0;
 }
+
+// int main(int argc, char *argv[]) {
+//     GtkWidget *window;
+//     GtkWidget *grid;
+//     GtkWidget *login_button;
+
+//     // GTK 초기화
+//     gtk_init(&argc, &argv);
+
+//     // 메인 윈도우 생성
+//     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+//     gtk_window_set_title(GTK_WINDOW(window), "Login Window");
+//     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+//     // 그리드 생성
+//     grid = gtk_grid_new();
+//     gtk_container_add(GTK_CONTAINER(window), grid);
+
+//     // 로그인 버튼 생성 및 설정
+//     login_button = gtk_button_new_with_label("Login");
+//     g_signal_connect(login_button, "clicked", G_CALLBACK(on_login_button_clicked), NULL);
+//     gtk_grid_attach(GTK_GRID(grid), login_button, 1, 1, 1, 1);
+
+//     // 윈도우 크기 설정
+//     gtk_widget_set_size_request(window, 300, 200);
+
+//     // 윈도우를 보이게 함
+//     gtk_widget_show_all(window);
+
+//     // GTK 메인 루프 실행
+//     gtk_main();
+
+//     return 0;
+// }
