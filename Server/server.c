54130936ad64ac23f8ca5e721dfd40af84323cb9
dogcod <105590774+dogcod@users.noreply.github.com>
2023-12-14 15:17:16 +0900

    Update and rename transfer.c to server.c

---
+++ Server/server.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define PORT 12345
+#define BUFFER_SIZE 1024
+
+void error(const char *msg) {
+    perror(msg);
+    exit(1);
+}
+
+int main() {
+    int serverSocket, clientSocket;
+    struct sockaddr_in serverAddr, clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
+    char buffer[BUFFER_SIZE];
+
+    // 서버 소켓 생성
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket < 0)
+        error("Error opening socket");
+
+    // 소켓 주소 초기화
+    bzero((char *)&serverAddr, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(PORT);
+
+    // 소켓 바인딩
+    if (bind(serverSocket, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
+        error("Error on binding");
+
+    // 소켓 대기
+    listen(serverSocket, 5);
+
+    printf("Server listening on port %d...\n", PORT);
+
+    // 클라이언트 연결 대기
+    clientSocket = accept(serverSocket, (struct sockaddr *)&clientAddr, &clientAddrLen);
+    if (clientSocket < 0)
+        error("Error on accept");
+
+    printf("Client connected: %s\n", inet_ntoa(clientAddr.sin_addr));
+
+    // 파일 수신
+    FILE *file = fopen("received_file.txt", "wb");
+    if (!file)
+        error("Error opening file");
+
+    ssize_t bytesRead;
+    while ((bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0)) > 0) {
+        fwrite(buffer, 1, bytesRead, file);
+    }
+
+    fclose(file);
+    close(clientSocket);
+    close(serverSocket);
+
+    printf("File received successfully.\n");
+
+    return 0;
+}
